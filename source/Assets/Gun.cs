0105f1b8f64a1b9bfead332ab09ea2abeced47a4
oggyiu <ragbit@gmail.com>
2018-01-27 14:15:57 +0800

    box shooting added"

---
+++ source/Assets/Gun.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour {
+    public float radius = 0;
+    public float power = 0;
+    public float coolDown = 0;
+    public float upward = 0;
+    public bool isAutoWeapon = false;
+    public bool isMouseUp = true;
+    public GameObject prefabGunFire;
+    public float gunFireLife = 0.5f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetMouseButtonUp(0))
+        {
+            isMouseUp = true;
+        }
+        if(Input.GetMouseButtonDown(0))
+        {
+            isMouseUp = false;
+
+            Vector3 mouseScreenPos = Input.mousePosition;
+            Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mouseScreenPos);
+            GameObject gunFire = GameObject.Instantiate(prefabGunFire);
+            gunFire.transform.position = new Vector3(mouseWorldPos.x, mouseWorldPos.y, -1);
+            GameObject.DestroyObject(gunFire, 1.0f);
+            
+            Vector3 explosionPos = mouseWorldPos;
+            Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+            foreach (Collider hit in colliders)
+            {
+                Rigidbody rb = hit.GetComponent<Rigidbody>();
+
+                if (rb != null)
+                {
+                    rb.velocity = Vector3.zero;
+                    rb.angularVelocity = Vector3.zero;
+                    rb.AddExplosionForce(power, explosionPos, radius, upward);
+                    Debug.Log("hit " + rb.gameObject.name);
+                }
+            }
+        }
+
+		if(coolDown > 0)
+        {
+            coolDown -= Time.deltaTime;
+            if(coolDown <= 0)
+            {
+                coolDown = 0;
+            }
+        }
+	}
+
+    public bool isReady()
+    {
+        bool ready1 = coolDown <= 0;
+        bool ready2 = true;
+
+        if(!isAutoWeapon)
+        {
+            ready2 = isMouseUp;
+        }
+        return ready1 && ready2;
+    }
+}
---
+++ source/Assets/ShootingMgr.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingMgr : MonoBehaviour {
+    public Gun myGun;
+    public Gun[] guns;
+    public GameObject box;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 boxPosition = box.transform.position;
+        Vector3 cameraPosition = Camera.main.transform.position;
+        Camera.main.transform.position = new Vector3(cameraPosition.x, boxPosition.y, cameraPosition.z);
+    }
+}
