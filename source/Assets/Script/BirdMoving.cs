e02da5ed0b627890e6e4919ae1ff00a876745a1b
Jason Lam <nightboyx@gmail.com>
2018-01-28 00:11:15 +0800

    Add Bird

---
+++ source/Assets/Script/BirdMoving.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdMoving : MonoBehaviour {
+
+	private Transform Bird;
+	private bool StartFly = false;
+
+	// Use this for initialization
+	void Start () {
+		Bird = this.gameObject.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (StartFly) {
+			Bird.localPosition = new Vector3 (Bird.localPosition.x + 0.1f, Bird.localPosition.y, Bird.localPosition.z);
+		}
+
+		Debug.Log ("Start Flying " + Bird.localPosition.x);
+		if (Bird.localPosition.x > 30) {
+			DestroyObject(Bird);
+		}
+	}
+
+	private void OnTriggerEnter(Collider other) {
+		Debug.Log ("Start Flying");
+		StartFly = true;
+	}
+}
