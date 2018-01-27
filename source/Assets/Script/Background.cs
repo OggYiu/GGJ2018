8d06ce397d05f1be61e773e47b17f31bcc45fd9d
Jason Lam <nightboyx@gmail.com>
2018-01-27 16:41:19 +0800

    Add Scroll Background and Level 1

---
+++ source/Assets/Script/Background.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour {
+
+	public Transform Background1;
+	public Transform cam;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Background1.localPosition = new Vector3 (Background1.localPosition.x, cam.position.y, Background1.localPosition.z);
+	}
+}
