3e831b34432e65ca37ba3fdea76927d076db0ecc
Jason Lam <nightboyx@gmail.com>
2018-01-28 11:18:22 +0800

    Update UFO

--- source/Assets/Script/BirdMoving.cs
+++ source/Assets/Script/BirdMoving.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class BirdMoving : MonoBehaviour {
 
 	public bool Flip = false;
+	public bool IsUFO = false;
 	public float Speed = 10f;
 
 	private Transform Bird;
 	private bool StartFly = false;
+	private float VerticalSpeed = 5;
+	private float IniticalY;
+	private float vertical = 0;
 
 	// Use this for initialization
 	void Start () {
 		Bird = this.gameObject.transform;
+
+		if (IsUFO) {
+			IniticalY = Bird.localPosition.y;
+			vertical = VerticalSpeed;
+		}
 
 		if (Flip) {
 			Speed *= -1;
@@ -21,8 +30,14 @@ public class BirdMoving : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (IsUFO) {
+			if (Bird.localPosition.y > IniticalY + 3 || Bird.localPosition.y < IniticalY - 3) {
+				vertical *= -1;
+			}
+		}
+
 		if (StartFly) {
-			Bird.localPosition = new Vector3 (Bird.localPosition.x + (Speed * Time.deltaTime), Bird.localPosition.y, Bird.localPosition.z);
+			Bird.localPosition = new Vector3 (Bird.localPosition.x + (Speed * Time.deltaTime), Bird.localPosition.y + (vertical * Time.deltaTime), Bird.localPosition.z);
 		}
 
 		if (Bird.localPosition.x < -30 || Bird.localPosition.x > 30) {
