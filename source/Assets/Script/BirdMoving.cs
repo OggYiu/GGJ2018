f7f6a32317024c23d9e1ce43901bc0a9a7892643
Jason Lam <nightboyx@gmail.com>
2018-01-28 10:36:23 +0800

    Add UFO and Plane

--- source/Assets/Script/BirdMoving.cs
+++ source/Assets/Script/BirdMoving.cs
@@ -4,30 +4,34 @@ using UnityEngine;
 
 public class BirdMoving : MonoBehaviour {
 
+	public bool Flip = false;
+	public float Speed = 10f;
+
 	private Transform Bird;
 	private bool StartFly = false;
 
 	// Use this for initialization
 	void Start () {
 		Bird = this.gameObject.transform;
+
+		if (Flip) {
+			Speed *= -1;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (StartFly) {
-			Bird.localPosition = new Vector3 (Bird.localPosition.x + 0.1f, Bird.localPosition.y, Bird.localPosition.z);
+			Bird.localPosition = new Vector3 (Bird.localPosition.x + (Speed * Time.deltaTime), Bird.localPosition.y, Bird.localPosition.z);
 		}
 
-		//Debug.Log ("Start Flying " + Bird.localPosition.x);
-
-		if (Bird.localPosition.x > 30) {
+		if (Bird.localPosition.x < -30 || Bird.localPosition.x > 30) {
 			DestroyObject(Bird);
 			StartFly = false;
 		}
 	}
 
 	private void OnTriggerEnter(Collider other) {
-		//Debug.Log ("Start Flying");
 		StartFly = true;
 	}
 }
